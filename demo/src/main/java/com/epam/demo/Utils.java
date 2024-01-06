fa87c7ef603a5a17563b6f0adc155d424eb25841
Koleduke <Ctvm1234>
2024-01-06 15:36:15 +0300

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,14 @@
 package com.epam.demo;
 
+
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+       for(int i = 0; args.size()>i;i++){
+           if(Integer.getInteger(args.get(i))<0){return false;}
+       }
+       return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if(Integer.getInteger(str)>0){return true;}
+        return false;
     }
 }
